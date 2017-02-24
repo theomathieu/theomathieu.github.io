79ea78f1eaadf60352bdf5761457aaae7f343661
yoannbordeaux <yoann.berdoulat@devinci.fr>
2017-02-24 15:58:28 +0100

    Affichage des ws/...

--- JS/custom.js
+++ JS/custom.js
@@ -1,14 +1,13 @@
 function fonctionRequeteApi(url, elementID) {
-	<h1>elementID</h1>
     var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("demo").innerHTML = jsonPretty;
+        document.getElementById(elementID).innerHTML = jsonPretty;
     }
 	};
-xmlhttp.open("GET", "url", true);
+xmlhttp.open("GET", url, true);
 xmlhttp.send()
 };
 
